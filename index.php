b1002552fbf9e259ba86682245d0f37e54f0785b
julymaltceva <nephrit85@yandex.ru>
2018-11-13 21:51:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -63,10 +63,7 @@
 				<a href="/product-category/podarki/" class="activ_cat">Смотреть все</a>
 			</div>
 		</div>
-		<div class="row category">
-			<h2 class="category_title"><span>Букеты</span></h2>
-			<a href="/product-category/cvety/bukety/" class="activ_cat">Смотреть все</a>
-		</div>
+		
 		<div class="row">
 			<h2 class="h_c_form"><span>Индивидуальный букет</span></h2>
 		</div>	
