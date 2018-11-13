4aba4b26111481730e958eb988adf5e247837e27
julymaltceva <nephrit85@yandex.ru>
2018-11-14 02:29:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
 		</div>
 
 		<div class="row category">
-			<h2 class="category_title col-xs-5"><span>Букеты</span></h2>
+			<h2 class="category_title"><span>Букеты</span></h2>
 			<div class="col-xs-4"></div>
 			<a href="/product-category/cvety/bukety/" class="activ_cat col-xs-4">Смотреть все</a>
 		</div>
